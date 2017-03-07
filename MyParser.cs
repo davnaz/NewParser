1da425d11392353f9ff8501a3a5b2f50a86e7742
davnaz <davnazzz@gmail.com>
2017-03-07 18:39:27 +0300

    Прописаны первоначальные данные.

---
+++ MyParser.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class MyParser
+{
+    private static string baseUrlforParse = "";
+	public MyParser()
+	{
+	}
+}
--- NewParser/Program.cs
+++ NewParser/Program.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace NewParser
 {
+
     class Program
     {
+        private static string mainLink = "https://www.gumtree.com.au/s-property-for-rent/forrentby-ownr/c18364"; //start link for parsing
+        private static string gumtreeBaselink = "https://www.gumtree.com.au"; //gumtree.com.au
+        private static string dbConnectionString = "Data Source=NOTEBOOK;Initial Catalog=Offers;Integrated Security=True"; //string for database with Offers
+        private SqlConnection dbConnection;
+        private static string gumtreeLoginMail = "clfdynic5oep@mail.ru";
+        private static string gumtreePassword = "megapass!";
         static void Main(string[] args)
         {
-            Console.WriteLine("Manual commit!");
-            Console.ReadLine();
+           
         }
     }
 }
