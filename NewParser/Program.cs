8964f12e223cbcec7872c214bcf0156ff8f818a9
davnaz <davnazzz@gmail.com>
2017-03-20 11:56:05 +0300

    Init Try

--- NewParser/Program.cs
+++ NewParser/Program.cs
@@ -29,7 +29,38 @@ namespace NewParser
         {
             Init(out parser, out dbConnection);
             Authorizate();
+            getPhone();
+        }
 
+        private static void getPhone()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(gumtreeBaselink+ " /j-vac-phone-get.json?token=1142584461|1489997172292|6b42efbfbd116c|11efbfbdefbfbdefbfbdefbfbd|4c00efbfbdefbfbdefbfbd|65efbfbd4366efbfbd");
+            req.Method = "GET";
+            req.Timeout = 100000;
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+            req.KeepAlive = true;
+            req.Headers.Add(HttpRequestHeader.Cookie, "machId=8tjqFvgmDAzTpd7aZPQ6ko6Qkek23aNJuh6MQ0L01tSy8f416G_0VJPCZhLulwlCQA6Srh6YtwpbmZj8ZkxP_lYfWLBWVi9D6XJd; bs=%7B%22st%22%3A%7B%7D%7D; up=%7B%22ln%22%3A%22302987873%22%2C%22ls%22%3A%22l%3D0%26r%3D0%26sv%3DLIST%26sf%3Ddate%22%2C%22lsh%22%3A%22l%3D0%26c%3D18364%26r%3D0%26sv%3DLIST%26rentals.forrentby_s%3Downr%26sf%3Ddate%22%2C%22lbh%22%3A%22l%3D0%26c%3D18364%26r%3D0%26sv%3DLIST%26rentals.forrentby_s%3Downr%26sf%3Ddate%22%7D; __utmt_siteTracker=1; __gads=ID=5ae0f0d7c464bcf1:T=1489057452:S=ALNI_MaRx14q9pz3OlJD-agyLiPRPgJSQg; _gat=1; sc=k6fIThLZKoQTphDwQ4jV; __utma=160852194.1931519358.1489057448.1489057448.1489057448.1; __utmb=160852194.2.10.1489057448; __utmc=160852194; __utmz=160852194.1489057448.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ga=GA1.3.1931519358.1489057448; ki_t=1489057454836%3B1489057454836%3B1489057469432%3B1%3B2; ki_r=; _gali=btn-submit-login");
+            req.Host = "www.gumtree.com.au";
+            if (cookies != null)
+            {
+                req.CookieContainer = cookies; //самый важный пункт: сюда добавляем печеньку с идентификатором авторизации
+            }
+            req.AllowAutoRedirect = true;
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            System.IO.Stream ReceiveStream = res.GetResponseStream();
+            System.IO.StreamReader sr2 = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8);
+            //Кодировка указывается в зависимости от кодировки ответа сервера
+            Char[] read = new Char[256];
+            int count = sr2.Read(read, 0, 256);
+            string htmlString = String.Empty;
+            while (count > 0)
+            {
+                String str = new String(read, 0, count);
+                htmlString += str;
+                count = sr2.Read(read, 0, 256);
+            }
+            Console.WriteLine();
         }
 
         private static void Authorizate()
@@ -94,7 +125,7 @@ namespace NewParser
             cookies.Add(gumtreeUri,c); //получили нашу куки, теперь у нас есть ИД сессии
         }//используем данные, которые указаны в начале класса для авторизации на сайте
 
-        private static string GetHtml(string link) //получаем страницу с 
+        private static string GetHtml(string link) //получаем страницу в виде строки, которую будем парсить
         {
             //а Теперь повторим запрос, но теперь вложим печеньку внутрь запроса с идентификатором сессии
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(link);
